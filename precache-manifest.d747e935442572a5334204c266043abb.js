4ac22e2f4d00c944c79799c28754081755a27b35
raminder goraya <raminder.goraya@gmail.com>
2018-10-08 01:58:50 -0700

    Updates

---
+++ precache-manifest.d747e935442572a5334204c266043abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b724a9097f395bdef458",
+    "url": "/react-crud-contacts/static/js/runtime~main.b724a909.js"
+  },
+  {
+    "revision": "bb0c121f9fe1b526526d",
+    "url": "/react-crud-contacts/static/js/main.bb0c121f.chunk.js"
+  },
+  {
+    "revision": "4048740b02edbfb58e93",
+    "url": "/react-crud-contacts/static/js/1.4048740b.chunk.js"
+  },
+  {
+    "revision": "bb0c121f9fe1b526526d",
+    "url": "/react-crud-contacts/static/css/main.b26f3c7e.chunk.css"
+  },
+  {
+    "revision": "4048740b02edbfb58e93",
+    "url": "/react-crud-contacts/static/css/1.e87259fd.chunk.css"
+  },
+  {
+    "revision": "81227f31b3176f7125e3a447a1cd0a66",
+    "url": "/react-crud-contacts/index.html"
+  }
+];
